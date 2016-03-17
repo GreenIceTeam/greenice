e5ab72fba4346b1de95cbcb74254379cadf04794
thierry <thierrylalanda@gmail.com>
2016-03-17 19:15:41 +0100

    creation du model signupForm  et actionSignup

--- frontend/modules/member/controllers/MemberController.php
+++ frontend/modules/member/controllers/MemberController.php
@@ -9,4 +9,20 @@ class MemberController extends \yii\web\Controller
         return $this->render('index');
     }
 
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+        if ($model->load(Yii::$app->request->post())) {
+            if ($user = $model->signup()) {
+                if (Yii::$app->getUser()->login($user)) { 
+                   return $this->render('home');
+                }
+            }
+        }
+
+        return $this->render('signup', [
+            'model' => $model,
+        ]);
+    }
+
 }
--- frontend/modules/member/models/SignupForm.php
+++ frontend/modules/member/models/SignupForm.php
@@ -13,6 +13,15 @@ class SignupForm extends Model
     public $username;
     public $email;
     public $password;
+    public $id_domaine;
+    public $id_sous_dom;
+    public $nom;
+    public $prenom;
+    public $sexe;
+    public $date_naiss;
+    public $ville;
+    public $role;
+
 
     /**
      * @inheritdoc
@@ -32,7 +41,24 @@ class SignupForm extends Model
             ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
 
             ['password', 'required'],
-            ['password', 'string', 'min' => 6],
+            ['password', 'string', 'min' => 8],
+
+            ['nom', 'required'],
+            ['nom', 'string','min'=>3,'max'=>20],
+
+            ['prenom', 'required'],
+            ['prenom', 'string','min'=>3,'max'=>20],
+
+            ['sexe', 'required'],
+            
+            ['ville', 'required'],
+            ['ville', 'string','max'=>20],
+
+            ['role', 'required'],
+            ['role', 'string','max'=>255],
+
+            ['DateDeNaissance', 'required'],
+            ['DateDeNaissance', 'date'],
         ];
     }
 
@@ -46,7 +72,17 @@ class SignupForm extends Model
         if ($this->validate()) {
             $user = new User();
             $user->username = $this->username;
+            $user->nom = $this->nom;
+            $user->prenom = $this->prenom;
             $user->email = $this->email;
+            $user->id_domaine = $this->domaine;
+            $user->id_sous_dom = $this->id_sous_dom;
+            $user->sexe = $this->sexe;
+            $user->date_naiss = $this->date_naiss;
+            $user->ville = $this->ville;
+            $user->role = $this->role;
+            $user->date_insc = NOW();
+            $user->last_active_time = Date();
             $user->setPassword($this->password);
             $user->generateAuthKey();
             if ($user->save()) {
