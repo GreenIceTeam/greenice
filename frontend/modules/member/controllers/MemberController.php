ac6ad2d187254b127ff1db238ef325c5b7ec738e
landry <landrybb93@gmail.com>
2016-03-19 17:44:18 +0100

    connexion et deconnexion d'un membre

--- frontend/modules/member/controllers/MemberController.php
+++ frontend/modules/member/controllers/MemberController.php
@@ -1,12 +1,36 @@
 <?php
 
 namespace frontend\modules\member\controllers;
+use Yii;
+use common\models\LoginForm;
 
 class MemberController extends \yii\web\Controller
 {
     public function actionIndex()
     {
         return $this->render('index');
+    }
+    public function actionLogin()
+    {
+        if (!\Yii::$app->user->isGuest) {
+        return $this->goHome(); //render('index');
+     }
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack(); //render('index');
+        } else {
+            return $this->render('login', [
+                'model' => $model,
+            ]);
+        }
+  
+    }
+
+public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
     }
 
 }
