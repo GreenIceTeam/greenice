67bd11a2bd8fff23b47bb55a1af8d8407faa48b9
Jospin Tedjou <tedjoujospin@yahoo.fr>
2016-03-20 11:23:29 +0100

    migration top insert first admin

--- backend/modules/admin/controllers/AdminController.php
+++ backend/modules/admin/controllers/AdminController.php
@@ -50,28 +50,13 @@ public function actionLogout()
             ->count();
     if ($count == 1) {
         
-    return $this->render('index');
+    return $this->render('index'); 
     }
     
-    $user=new User(); 
-    $user->email= 'jospintedjou@yahoo.fr';
-    $user->nom= 'admin1';
     
-    $user->date_insc= date('Y-M-D H:i:s');
- 
-    $user->sexe= 'H';
+    
    
-    $user->role='admin';
-    $user->username='admin1'  ;
-    $user->setPassword('Adm_pwd?');
-    //$user->id_domaine=1;
-    //$user->id_sous_dom=1;
-  // $user->created_at = '1555239878';
-   //$user->updated_at = '1555239878';
-    $user->generateAuthKey();
-    
-   $user->save();
-    return ('signup validate');
+  
                
             
     }
