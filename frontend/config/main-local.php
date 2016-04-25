3b0f45070d25577b1cc18608d14a1526d41677f3
fritz kenne <kennefritzon@gmail.com>
2016-04-25 19:45:38 +0100

    design vues login et signup

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -4,29 +4,10 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'RgsBNsUCyIeALDAWJ4A8wCmVVB5p6t8Z'
+            'cookieValidationKey' => 'XdOGMxgNwquMYkhELsACOxkKCZ2jg-_9',
         ],
-            'session'=>[
-                                                    'name'=>'FRONTENDID4',
-                                                    'savePath'=>__DIR__.'/../tmp',
-            ],
-
-            'user'=>[
-                                                    'identityClass'=>'common\models\User',
-                                                    'enableAutoLogin'=>true,
-                                                    'identityCookie'=>[
-                                                                                    'name'=>'_frontendUser',
-                                                                                      'path'=>'/greenice/frontend/web'
-                                                    ]		
-            ]
-
-      ],
-
-'defaultRoute'=>'member/member/index'
-                 
-    
-    
-    ];
+    ],
+];
 
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
